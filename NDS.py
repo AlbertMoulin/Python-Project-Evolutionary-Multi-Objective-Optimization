3ee721531be994bf213ab76d62b138eb6957ae07
BuildTools <unconfigured@null.spigotmc.org>
2024-12-18 13:26:15 +0100

    Commented Non Dominated Sorting

--- NDS.py
+++ NDS.py
@@ -13,6 +13,7 @@ class Nds:
         
         Fronts = [[]]
         
+        # initialize D for all individuals
         for x in Q:
             D[x] = [0,[]]
             for y in Q:
@@ -21,21 +22,24 @@ class Nds:
                 if f(x)<f(y): 
                     D[x][0] +=1
             
+            # initialize first rank
             if D[x][0] == 0:
                 Fronts[0].append(x)
         
         i = 0
         while (True):
             FrontIPlusOne = []
+            # for every individual newly added to a rank we delete its dominations on the other (non-ranked) individuals
             for x in Fronts[i]:
-                
                 for y in D[x][1]:
                     D[y][0] -=1
-                    
+                    # if y is now non dominated we add it to the new front
                     if D[y][0] == 0:
                         FrontIPlusOne.append(y)
+            # if the new front is empty we have finished sorting
             if len(FrontIPlusOne) == 0:
                 return Fronts
+            # if not we continue
             Fronts.append(FrontIPlusOne)
             i += 1
             
@@ -51,6 +55,6 @@ if __name__ == "__main__":
         for fi in f:
             Qi.append(LOTZ.LOTZdeux(fi))
         Q.append(Qi)
-    print( F )
+    print(F)
     print(Q)
     
